dba390fe65f41597329e9a489cdd61b7779e855f
Tuannh1311 <Tuannh1311@outlook.com>
2022-04-06 21:04:18 +0700

    Huong quyettamchienthang

--- Basic.py
+++ Basic.py
@@ -5,3 +5,11 @@ x = [1,2,3,4,5]
 for i in x:
     print(i)
 # %%
+x = {'one':1,'two':2,'three':3,'four':4,'five':5}
+for k,v in x.items():
+    print('k is {}, x is {}'.format(k,v))
+# %%
+x=[1,2,3,4,5]
+for i in x:
+    print ('i is {}'.format(i))
+# %%
